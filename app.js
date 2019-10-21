2e036134930a367ca120a237091b81211703d232
LadyLilly <lady.vonderbockhoehe@gmail.com>
2019-10-21 23:40:42 +0200

    Added basic routes

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+/**
+ * Test page
+ */
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+/**
+ * GET - Login page
+ */
+app.get('/login', (req, res) => {
+    res.send('Login-Page');
+});
+
+/**
+ * GET - Register page
+ */
+app.get('/register', (req, res) => {
+    res.send('Register-Page');
+});
+
+/**
+ * POST - Register page
+ */
+app.post('/register', (req, res) => {
+    res.send('Register-Page');
+});
+
+/**
+ * This is the route for 404 errors
+ */
+app.get('*', (req, res) => {
+    res.send('Page not found', 404);
 });
 
 app.listen(3000, () => {
